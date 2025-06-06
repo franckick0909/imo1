457eb2f6acd1e56d17be46f81734e903a5ceb43a
Franck Chapelon <franckick2@gmail.com>
2025-06-07 00:07:58 +0200

    feat: ajout de la connexion sociale avec Google et GitHub dans les modales d'authentification - Intégration des boutons de connexion et gestion des authentifications via les fournisseurs sociaux.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -76,6 +76,16 @@ export const auth = betterAuth({
       await sendEmail(user.email, subject, template);
     },
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
   plugins: [
     emailOTP({
       async sendVerificationOTP({ email, otp, type }) {
